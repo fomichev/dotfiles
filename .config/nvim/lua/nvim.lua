45ddd5c35d441aebcb18c8db3df2d108d7ba83a0
Stanislav Fomichev <sdf@google.com>
2024-03-31 14:43:52 -0700

    ollama: custom prompts
    Signed-off-by: Stanislav Fomichev <sdf@google.com>

--- .config/nvim/lua/nvim.lua
+++ .config/nvim/lua/nvim.lua
@@ -32,6 +32,38 @@ require 'gen'.setup {
 	model = 'dolphin-mistral',
 }
 
+require 'gen'.prompts = {
+	Generate = {
+		prompt = "$input",
+		replace = true,
+	},
+	Comment = {
+		prompt = "Explain the following code snippet and write it as a comment, just output the final text without additional quotes around it:\n$text",
+		replace = true,
+	},
+	Ask = {
+		prompt = "Regarding the following text, $input:\n$text",
+	},
+	Summarize = {
+		prompt = "Summarize the following text:\n$text",
+	},
+	Rewrite = {
+		prompt = "Change the following text, $input, just output the final text without additional quotes around it:\n$text",
+		replace = true,
+	},
+	Review = {
+		prompt = "Review the following code and make concise suggestions:\n```$filetype\n$text\n```",
+	},
+	Fix_Grammar = {
+		prompt = "Modify the following text to improve grammar and spelling, just output the final text without additional quotes around it:\n$text",
+		replace = true,
+	},
+	Fix_Wording = {
+		prompt = "Modify the following text to use better wording, just output the final text without additional quotes around it:\n$text",
+		replace = true,
+	},
+}
+
 vim.keymap.set({ 'n', 'v' }, '<space>', ':Gen<CR>')
 
 -- Disable all diagnositcs
