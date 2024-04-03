768c8491dad87e8375465249a4c42c03b67d3680
Stanislav Fomichev <s@fomichev.me>
2024-04-03 00:30:36 -0700

    nvim ' to "
    Signed-off-by: Stanislav Fomichev <s@fomichev.me>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -165,9 +165,9 @@ require("lazy").setup({
 	"tinted-theming/base16-vim",
 	"neovim/nvim-lspconfig",
 	{
-		'nvim-telescope/telescope.nvim',
-		branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim' }
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" }
 	},
 	{
 		"nvim-telescope/telescope-frecency.nvim",
@@ -203,14 +203,14 @@ require("lazy").setup({
 			})
 		end
 	},
-	'hrsh7th/cmp-nvim-lsp',
-	'hrsh7th/cmp-buffer',
-	'hrsh7th/cmp-path',
-	'hrsh7th/cmp-cmdline',
-	'hrsh7th/nvim-cmp',
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/nvim-cmp",
 })
 
-local cmp = require'cmp'
+local cmp = require"cmp"
 
 cmp.setup({
 	snippet = {
@@ -220,40 +220,40 @@ cmp.setup({
 	},
 	window = {},
 	mapping = cmp.mapping.preset.insert({
-		['<C-Space>'] = cmp.mapping.complete(),
+		["<C-Space>"] = cmp.mapping.complete(),
 	}),
 	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
+		{ name = "nvim_lsp" },
 		}, {
-			{ name = 'buffer' },
+			{ name = "buffer" },
 	})
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require'lspconfig'.clangd.setup{
+require"lspconfig".clangd.setup{
 	capabilities = capabilities
 }
 
-require'lspconfig'.rust_analyzer.setup{
+require"lspconfig".rust_analyzer.setup{
 	capabilities = capabilities
 }
 
-vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 	callback = function(ev)
 		-- Enable completion triggered by <c-x><c-o>
-		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
 		local opts = { buffer = ev.buf }
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-		vim.keymap.set({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, opts)
-		vim.keymap.set('n', '<leader>f', function()
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<leader>f", function()
 			vim.lsp.buf.format { async = true }
 		end, opts)
 	end,
@@ -263,11 +263,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
 vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
 
 -- Fuzzy finder
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
 
 vim.keymap.set("n", "<leader>r", function()
 	require("telescope").extensions.frecency.frecency {
